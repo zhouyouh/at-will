ca60a7d28f2175b458198a1532c5f75fccea2785
zhouyh <2832610678@qq.com>
2017-10-07 18:49:36 +0800

    local commit

---
+++ git/php.mm
@@ -0,0 +1,29 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1507372632758" ID="ID_1102729125" MODIFIED="1507372712166" TEXT="phptutorial">
+<node CREATED="1507372719368" ID="ID_1277365119" MODIFIED="1507372739074" POSITION="right" TEXT="&#x73af;&#x5883;&#x5b89;&#x88c5;">
+<node CREATED="1507372748774" ID="ID_1289831715" MODIFIED="1507373276238">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1&#12289;PHP Fatal error:&#160;&#160;Call to undefined function curl_init()&#160;&#65306;
+    </p>
+    <p>
+      extension_dir &#25351;&#21521;&#36335;&#24452;&#27809;&#26377;&#38169;&#65292;extension=php_curl.dll&#21069;&#38754;&#30340;&#20998;&#21495;
+    </p>
+    <p>
+      &#20063;&#24050;&#32463;&#21435;&#25481;&#65292;&#29978;&#33267;&#20063;&#23558;libeay32.dll&#12289;ssheay32.dll&#12289;php5ts.dll&#19982;php_curl.dll&#20063;&#25335;&#36125;&#21040;system32&#19982;SysWOW64&#30446;&#24405;&#19979;&#65292;&#37325;&#21551;apche&#21518;&#36824;&#26159;&#27809;&#26377;&#29992;&#12290;&#21407;&#26469;&#22312;apache&#30340;bin&#38656;&#35201;&#36825;&#19977;&#20010;&#25991;&#20214;&#65306;
+    </p>
+    <p>
+      <code>libeay32.dll</code>&#12289;<code>libssh2.dll</code>&#12289;<code>ssleay32.dll</code>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
